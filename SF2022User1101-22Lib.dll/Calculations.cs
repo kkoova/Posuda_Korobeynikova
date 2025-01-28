b61ef11ebf3fe6b6a121b811a4ee496cf9a56c1a
Korobeynikova <Korobeynikova@B447B25>
2025-01-28 12:26:25 +0300

    Тесты и билиотека

--- SF2022User1101-22Lib.dll/Calculations.cs
+++ SF2022User1101-22Lib.dll/Calculations.cs
@@ -1,30 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SF2022User1101_22Lib.dll
 {
     public class Calculations
     {
-        public string AvailablePeriods(TimeSpan[] startTimes,
+        public string[] SetDataTrue(string startTimesAndDurations, 
+            string beginWorkingTimeAndEndWorkingTime, int consultationTime)
+        {
+            TimeSpan[] startTimes = new TimeSpan[24];
+            int[] durations = new int[24];
+            TimeSpan beginWorkingTime = new TimeSpan();
+            TimeSpan endWorkingTime = new TimeSpan();
+
+            string[] vvod = startTimesAndDurations.Split(',');
+            for(int i = 0; i < vvod.Length; i++)
+            {
+                string[] parts = vvod[i].Split(' ');
+
+                durations[i] = Int16.Parse(parts[1]);
+                startTimes[i] = TimeSpan.Parse(parts[0]);
+            }
+
+            string[] vvodTime = beginWorkingTimeAndEndWorkingTime.Split('-');
+            beginWorkingTime = TimeSpan.Parse(vvodTime[0]);
+            endWorkingTime = TimeSpan.Parse(vvodTime[1]);
+
+            var result = AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+            return result;
+        }
+        private string[] AvailablePeriods(TimeSpan[] startTimes,
             int[] durations,
             TimeSpan beginWorkingTime,
             TimeSpan endWorkingTime,
             int consultationTime)
         {
-            var vvod = "";
-            var booll = false;
-            var i = 0;
+            List<string> availableIntervals = new List<string>();
 
-            Console.WriteLine(vvod);
+            var sortedOccupiedIntervals = startTimes.Select((startTime, index) => new { StartTime = startTime, Duration = durations[index], Index = index })
+                                                  .OrderBy(x => x.StartTime).ToList();
 
-            if (vvod.Length < 8) return null;
-            string[] parts = vvod.Split(' ');
+            TimeSpan lastEndTime = TimeSpan.Zero;
+            bool isFirstInterval = true;
 
-            startTimes[i] = TimeSpan.Parse(parts[0]);
-            durations[i] = Int16.Parse(parts[1]);
+            foreach (var occupiedInterval in sortedOccupiedIntervals)
+            {
+                TimeSpan intervalStart = occupiedInterval.StartTime;
+                TimeSpan intervalDuration = TimeSpan.FromMinutes(occupiedInterval.Duration);
 
-            i++;
-            
-            return 
+                if (!isFirstInterval && lastEndTime < intervalStart)
+                {
+                    TimeSpan availableTime = intervalStart - lastEndTime;
+                    AddAvailableInterval(availableIntervals, availableTime);
+                }
+
+                lastEndTime = intervalStart + intervalDuration;
+
+                isFirstInterval = false;
+            }
+
+            if (lastEndTime < endWorkingTime)
+            {
+                TimeSpan remainingTime = endWorkingTime - lastEndTime;
+                AddAvailableInterval(availableIntervals, remainingTime);
+            }
+
+            return availableIntervals.ToArray();
+        }
+
+        private static void AddAvailableInterval(List<string> availableIntervals, TimeSpan availableTime)
+        {
+            string formattedTime = availableTime.ToString(@"hh\:mm");
+            availableIntervals.Add(formattedTime);
         }
     }
 }
---
+++ SF2022User1101-22Lib.dllTests/CalculationsTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SF2022User1101_22Lib.dll.Tests
+{
+    [TestClass()]
+    public class CalculationsTests
+    {
+        [TestMethod()]
+        public void SetDataTrueTest()
+        {
+            var calc = new Calculations();
+
+            var result = calc.SetDataTrue("09:00 30","08:00-10:00",30);
+        }
+
+        [TestMethod()]
+        public void SetDataTrueTestTwo()
+        {
+            var calc = new Calculations();
+
+            var result = calc.SetDataTrue("09:00 30", "08:00-14:00", 30);
+        }
+
+        [TestMethod()]
+        public void SetDataTrueTestTree()
+        {
+            var calc = new Calculations();
+
+            var result = calc.SetDataTrue("09:00 30", "08:00-11:00", 30);
+        }
+
+        [TestMethod()]
+        public void SetDataTrueTestFour()
+        {
+            var calc = new Calculations();
+
+            var result = calc.SetDataTrue("09:00 30", "08:00-19:00", 30);
+        }
+
+        [TestMethod()]
+        public void SetDataTrueTestFive()
+        {
+            var calc = new Calculations();
+
+            var result = calc.SetDataTrue("09:00 30", "08:00-23:00", 30);
+        }
+    }
+}
